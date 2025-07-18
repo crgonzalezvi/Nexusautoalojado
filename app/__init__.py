ba72c2f498cf0ab1eae6a14ca2be43680fd885e3
crgonzalezvi <crgonzalezvi@unal.edu.co>
2025-07-17 22:24:58 -0500

    Actualización 5 Prometheus

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from .routes import router as main_router
 from .metrics import setup_metrics
 
-app = FastAPI(
-    title="Store API",
-    description="API para gestionar clientes, productos y ventas",
-    version="1.0.0"
-)
+def create_app():
+    app = FastAPI(
+        title="Store API",
+        description="API para gestionar clientes, productos y ventas",
+        version="1.0.0"
+    )
 
-app.include_router(main_router)
-setup_metrics(app)
+    app.include_router(main_router)
+    setup_metrics(app)
+
+    return app
